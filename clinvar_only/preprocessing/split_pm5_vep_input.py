66c2c8b8ced1524c966fd643b4e08f545889d59a
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2022-01-31 23:22:50 -0800

    Create split_pm5_vep_input.py

---
+++ clinvar_only/preprocessing/split_pm5_vep_input.py
@@ -0,0 +1,22 @@
+import os
+
+df = pd.read_csv("/net/data/aasubs/clinvar_only/dup_files/moderate_vep_input.txt", sep = "\t", index_col = False, header = None)
+
+start = 0
+end = 7350
+
+for i in range(21):
+	with open(f"/net/data/aasubs/clinvar_only/vep/{i}_moderate_vep_input.txt", "a") as f:
+		if i == 20:
+			for j in range(start, len(df)):
+				f.write(str(df.iloc[j][0]))
+				f.write("\n")
+		else:
+			for j in range(start, end):
+				f.write(str(df.iloc[j][0]))
+				f.write("\n")
+	f.close()
+
+	start += 7350
+	end += 7350
+
