f7c43c66193008c7b76fcdff52b3ac253aaeeb44
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2022-03-20 19:24:47 -0700

    Update 4_find_pm5_discordant_classifications.py

--- clinvar_only/analysis/4_find_pm5_discordant_classifications.py
+++ clinvar_only/analysis/4_find_pm5_discordant_classifications.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 		nameDf = pd.read_csv("/net/data/aasubs/clinvar_only/dup_files/pm5_all_subs.csv")
 
 		df = pd.read_csv(f"/net/data/aasubs/clinvar_only/annotated/moderate_annotated_dup_subs.csv")
-		df = df[df["simple_name"].isin(nameDf["name"])].sort_values(by = ["simple_name"]).reset_index(drop = True)
+		df = df[df["simple_name"].isin(nameDf["name"])].sort_values(by = ["vus_aa_sub_name"]).reset_index(drop = True)
 		df.drop(df.filter(regex="Unname"),axis=1, inplace=True)
 
 		ndDf = df.drop_duplicates(subset = ["aa_half_name"]).reset_index(drop = True)
@@ -57,7 +57,7 @@ if __name__ == "__main__":
 		finalVusSepDf.columns = vus_columns_sep
 
 		finalSepDifDf = pd.concat([finalClasSepDf, finalVusSepDf], axis = 1)
-		finalSepDifDfND = finalSepDifDf.drop_duplicates(subset = [f"{clas_v2}_aa_sub_name"]).drop_duplicates(subset = ["vus_aa_sub_name"])
-
+		finalSepDifDf = finalSepDifDf.sort_values(by = ["vus_aa_sub_name"]).reset_index(drop = True)
+		
 		finalSepDifDf.to_csv(f"/net/data/aasubs/clinvar_only/results_new/moderate_{clas_v2}_analysis.csv")
 
