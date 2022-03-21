d6b63e6fa481c0fe00400a310199868fa7ef0584
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2022-03-20 18:52:53 -0700

    Create 8_combine_splice_vep_output.py

---
+++ dbnsfp_catalog/pvs1/preprocessing/8_combine_splice_vep_output.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+file_starters = []
+
+for i in range(101):
+	file_starters.append(f"{i}")
+
+vepDfFull = pd.DataFrame()
+
+
+for x in file_starters:
+	part_file_path = f"/net/data/aasubs/dbnsfp_catalog/pvs1/{x}_dbn_splice_vep_output.vcf"
+	vepDfPart = pd.read_csv(part_file_path, sep = "\t", skiprows = 105)
+
+	if x == "0":
+		vepDfFull = pd.DataFrame(columns = vepDfPart.columns)
+
+	vepDfFull = pd.concat([vepDfFull, vepDfPart], ignore_index = True)
+
+
+vepDfFull.to_csv("/net/data/aasubs/dbnsfp_catalog/pvs1/dbnsfp_splice_vep_output.csv")
