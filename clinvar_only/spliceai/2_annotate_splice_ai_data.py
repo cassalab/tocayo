c065d6e653fe84c44a268db61ea3a2da40a3b816
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2022-03-20 18:26:24 -0700

    Update 2_annotate_splice_ai_data.py

--- clinvar_only/spliceai/2_annotate_splice_ai_data.py
+++ clinvar_only/spliceai/2_annotate_splice_ai_data.py
@@ -10,11 +10,11 @@ for evidence in evidenceList:
 		clas = classificationList[i]
 		clas_v2 = classificationList_v2[i]
     
-    		#SpliceAI data retrieved from VEP Web Interface
 		df = pd.read_csv(f"/Users/vineel/Desktop/{evidence}_{clas_v2}_analysis.csv")
 		df[f"{clas_v2}_simple_name"] = df[f"{clas_v2}_Chromosome"].map(str) + "-" + df[f"{clas_v2}_Start"].map(str) + "-" + df[f"{clas_v2}_AlternateAlleleVCF"].map(str)
 		df["vus_simple_name"] = df["vus_Chromosome"].map(str) + "-" + df["vus_Start"].map(str) + "-" + df["vus_AlternateAlleleVCF"].map(str)
-
+		
+		#SpliceAI data retrieved from VEP Web Interface
 		addClasDf = pd.read_csv(f"/Users/vineel/Documents/sa_vep/{evidence}_{clas_v2}_{clas_v2}_output.txt", sep = "\t")
 		addVusDf = pd.read_csv(f"/Users/vineel/Documents/sa_vep/{evidence}_{clas_v2}_vus_output.txt", sep = "\t")
 
@@ -28,10 +28,10 @@ for evidence in evidenceList:
 		addVusDf["simple_name"] = addVusDf["vep_chrom"].map(str) + "-" + addVusDf["vep_pos"].map(str) + "-" + addVusDf["Allele"].map(str)
 
 		addClasDf = addClasDf[(addClasDf["SpliceAI_pred_DS_AG"] != "-") | (addClasDf["SpliceAI_pred_DS_AL"] != "-") | (addClasDf["SpliceAI_pred_DS_DG"] != "-") | (addClasDf["SpliceAI_pred_DS_DL"] != "-")]
-    addClasDf = addClasDf.drop_duplicates(subset = ["simple_name"])
+    		addClasDf = addClasDf.drop_duplicates(subset = ["simple_name"])
 
 		addVusDf = addVusDf[(addVusDf["SpliceAI_pred_DS_AG"] != "-") | (addVusDf["SpliceAI_pred_DS_AL"] != "-") | (addVusDf["SpliceAI_pred_DS_DG"] != "-") | (addVusDf["SpliceAI_pred_DS_DL"] != "-")]
-    addVusDf = addVusDf.drop_duplicates(subset = ["simple_name"])
+    		addVusDf = addVusDf.drop_duplicates(subset = ["simple_name"])
 
 		addClasDf["sa_score"] = addClasDf[["SpliceAI_pred_DS_AG", "SpliceAI_pred_DS_AL", "SpliceAI_pred_DS_DG", "SpliceAI_pred_DS_DL"]].astype(float).max(axis=1)
 		addVusDf["sa_score"] = addVusDf[["SpliceAI_pred_DS_AG", "SpliceAI_pred_DS_AL", "SpliceAI_pred_DS_DG", "SpliceAI_pred_DS_DL"]].astype(float).max(axis=1)
