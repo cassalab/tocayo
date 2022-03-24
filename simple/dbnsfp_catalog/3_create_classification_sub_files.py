7dc77224f80b25b9507a151193fcd1430a48464c
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2022-03-23 21:24:35 -0700

    Update 3_create_classification_sub_files.py

--- simple/dbnsfp_catalog/3_create_classification_sub_files.py
+++ simple/dbnsfp_catalog/3_create_classification_sub_files.py
@@ -11,7 +11,12 @@ for evidence in evidenceList:
 		clas = classificationList[i]
 		clas_v2 = classificationList_v2[i]
 		
-		cvDf = pd.read_csv(f"/net/data/aasubs/dbnsfp_catalog/{evidence}_filtered_potential_entries_full.csv")
+		cvDf = pd.read_csv(f"/net/data/aasubs/dbnsfp_catalog/{evidence}_cv_potential_entries.csv")
+		dbDf = pd.read_csv(f"/net/data/aasubs/dbnsfp_catalog/{evidence}_cv_potential_entries.csv")
+		
+		dbDf["aa_sub_name"] = cvDf["aa_sub_name"]
+		
+		
 
 		df = pd.read_csv(f"/net/data/aasubs/dbnsfp_catalog/filtered/{evidence}_filtered_potential_entries_full.csv")
 
