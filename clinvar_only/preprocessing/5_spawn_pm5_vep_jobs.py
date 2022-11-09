74ba66fd4b4d736afb562c87f2c4a45febbfb2a2
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2022-11-08 17:26:29 -0800

    Update 5_spawn_pm5_vep_jobs.py

--- clinvar_only/preprocessing/5_spawn_pm5_vep_jobs.py
+++ clinvar_only/preprocessing/5_spawn_pm5_vep_jobs.py
@@ -1,14 +1,13 @@
 import os
 
-files_left = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
 
-for x in files_left:
-	input_path = f"/net/data/aasubs/clinvar_only/vep/{x}_moderate_vep_input.txt"
-	output_path = f"/net/data/aasubs/clinvar_only/vep/{x}_moderate_vep_output.txt"
+for i in range(47):
+	input_path = f"/net/data/aasubs/clinvar_only/vep/{i}_moderate_vep_input.txt"
+	output_path = f"/net/data/aasubs/clinvar_only/vep/{i}_moderate_vep_output.txt"
 	plugin_command = "--plugin CADD,/net/data/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz"
 	assembly = "GRCh38"
 
-	job_path = "/net/data/aasubs/clinvar_only/vep/j" + x + "_vep.sh"
+	job_path = "/net/data/aasubs/clinvar_only/vep/j" + str(i) + "_vep.sh"
 	with open(job_path, "w") as f:
  		command = f"""
  		vep --cache --dir /net/data/vep --assembly {assembly} --tab --no_stats -i {input_path} -o {output_path} --everything --refseq {plugin_command}
