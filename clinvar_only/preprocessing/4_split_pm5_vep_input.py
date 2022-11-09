59070227286498b001a508b98b6e89d4d3542728
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2022-11-08 16:55:56 -0800

    Update 4_split_pm5_vep_input.py

--- clinvar_only/preprocessing/4_split_pm5_vep_input.py
+++ clinvar_only/preprocessing/4_split_pm5_vep_input.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import os
 
-df = pd.read_csv("/net/data/aasubs/update_11-06-22/clinvar_only/dup_files/moderate_vep_input.txt", sep = "\t", index_col = False, header = None)
+df = pd.read_csv("/net/data/aasubs/clinvar_only/dup_files/moderate_vep_input.txt", sep = "\t", index_col = False, header = None)
 
 start = 0
 end = 5000
 
 for i in range(47):
-	with open(f"/net/data/aasubs/update_11-06-22/clinvar_only/vep/{i}_moderate_vep_input.txt", "a") as f:
+	with open(f"/net/data/aasubs/clinvar_only/vep/{i}_moderate_vep_input.txt", "a") as f:
 		if i == 46:
 			for j in range(start, len(df)):
 				f.write(str(df.iloc[j][0]))
