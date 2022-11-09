6f531e2bc2dcca04fb9fe765ae26eb816cb96e06
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2022-11-08 16:54:58 -0800

    Update 4_split_pm5_vep_input.py

--- clinvar_only/preprocessing/4_split_pm5_vep_input.py
+++ clinvar_only/preprocessing/4_split_pm5_vep_input.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import os
 
-df = pd.read_csv("/net/data/aasubs/clinvar_only/dup_files/moderate_vep_input.txt", sep = "\t", index_col = False, header = None)
+df = pd.read_csv("/net/data/aasubs/update_11-06-22/clinvar_only/dup_files/moderate_vep_input.txt", sep = "\t", index_col = False, header = None)
 
 start = 0
-end = 7350
+end = 5000
 
-for i in range(21):
-	with open(f"/net/data/aasubs/clinvar_only/vep/{i}_moderate_vep_input.txt", "a") as f:
-		if i == 20:
+for i in range(47):
+	with open(f"/net/data/aasubs/update_11-06-22/clinvar_only/vep/{i}_moderate_vep_input.txt", "a") as f:
+		if i == 46:
 			for j in range(start, len(df)):
 				f.write(str(df.iloc[j][0]))
 				f.write("\n")
@@ -18,6 +18,5 @@ for i in range(21):
 				f.write("\n")
 	f.close()
 
-	start += 7350
-	end += 7350
-
+	start += 5000
+	end += 5000
