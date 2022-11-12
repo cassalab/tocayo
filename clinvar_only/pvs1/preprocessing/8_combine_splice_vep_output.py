2c6540e36f148e915a3372de1d68fe83a7bacb2b
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2022-11-11 16:17:16 -0800

    Update 8_combine_splice_vep_output.py

--- clinvar_only/pvs1/preprocessing/8_combine_splice_vep_output.py
+++ clinvar_only/pvs1/preprocessing/8_combine_splice_vep_output.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 
-file_starters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"]
-
 vepDfFull = pd.DataFrame()
 
 
-for x in file_starters:
-	part_file_path = f"/net/data/aasubs/clinvar_only/pvs1/{x}_splice_vep_output.vcf"
+for i in range(26):
+
+	part_file_path = f"/net/data/aasubs/clinvar_only/pvs1/{i}_splice_vep_output.vcf"
 	vepDfPart = pd.read_csv(part_file_path, sep = "\t", skiprows = 105)
 
-	if x == "0":
+	if i == 0:
+
 		vepDfFull = pd.DataFrame(columns = vepDfPart.columns)
 
 	vepDfFull = pd.concat([vepDfFull, vepDfPart], ignore_index = True)
