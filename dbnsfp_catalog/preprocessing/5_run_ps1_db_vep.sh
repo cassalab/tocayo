09e14cd39fadc06b663064725c5fa9f7ea686e2e
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2023-01-09 18:12:07 -0800

    Update 5_run_ps1_db_vep.sh

--- dbnsfp_catalog/preprocessing/5_run_ps1_db_vep.sh
+++ dbnsfp_catalog/preprocessing/5_run_ps1_db_vep.sh
@@ -1,1 +1,1 @@
-vep --cache --dir /net/data/vep --assembly GRCh38 --tab --no_stats -i /net/data/aasubs/dbnsfp_catalog/vep/strong_db_vep_input.txt -o /net/data/aasubs/dbnsfp_catalog/vep/strong_db_vep_output.txt --everything --refseq --plugin CADD,/net/data/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz
+vep --cache --dir /net/data/vep --assembly GRCh38 --tab --no_stats -i /net/data/aasubs/dbnsfp_catalog/vep/strong_db_vep_input.vcf -o /net/data/aasubs/dbnsfp_catalog/vep/strong_db_vep_output.txt --everything --refseq --plugin CADD,/net/data/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz
