443d96c3f928bf6a2319ce6a03da5996953f7367
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2023-01-13 22:44:05 -0800

    Update 4_filter_unique_evidence.py

--- dbnsfp_catalog/analysis/4_filter_unique_evidence.py
+++ dbnsfp_catalog/analysis/4_filter_unique_evidence.py
@@ -4,8 +4,8 @@ evidenceList = ["strong", "moderate"]
 
 for evidence in evidenceList:
 
-	plpDfOriginal = pd.read_csv(f"dbnsfp_{evidence}_plp_analysis.csv")
-	blbDfOriginal = pd.read_csv(f"dbnsfp_{evidence}_blb_analysis.csv")
+	plpDfOriginal = pd.read_csv(f"/net/data/aasubs/dbnsfp_catalog/classified/dbnsfp_{evidence}_plp_analysis.csv")
+	blbDfOriginal = pd.read_csv(f"/net/data/aasubs/dbnsfp_catalog/classified/dbnsfp_{evidence}_blb_analysis.csv")
 
 	plpDf = plpDfOriginal[plpDfOriginal["cv_aa_half_name"].isin(blbDfOriginal["cv_aa_half_name"]) == False]
 	blbDf = blbDfOriginal[blbDfOriginal["cv_aa_half_name"].isin(plpDfOriginal["cv_aa_half_name"]) == False]
@@ -15,5 +15,5 @@ for evidence in evidenceList:
 	plpDf = plpDf.sort_values(by = ["cv_aa_sub_name"]).reset_index(drop = True)
 	blbDf = blbDf.sort_values(by = ["cv_aa_sub_name"]).reset_index(drop = True)
 
-	plpDf.to_csv(f"dbnsfp_{evidence}_plp_analysis.csv")
-	blbDf.to_csv(f"dbnsfp_{evidence}_blb_analysis.csv")
+	plpDf.to_csv(f"/net/data/aasubs/dbnsfp_catalog/unique/dbnsfp_{evidence}_plp_analysis.csv")
+	blbDf.to_csv(f"/net/data/aasubs/dbnsfp_catalog/unique/dbnsfp_{evidence}_blb_analysis.csv")
