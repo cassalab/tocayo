7a7aefe2395cd7aa509a010421e3bfdb10c63ea7
vbhat4 <98800485+vbhat4@users.noreply.github.com>
2023-01-18 21:07:37 -0800

    Update 1_get_potential_stop_entries.py

--- clinvar_only/pvs1/preprocessing/1_get_potential_stop_entries.py
+++ clinvar_only/pvs1/preprocessing/1_get_potential_stop_entries.py
@@ -9,9 +9,11 @@ def main():
 	df.drop(df.filter(regex="Unname"), axis=1, inplace=True)
 	df["aa_alt"] = df["aa_sub_name"].str[-3:]
 	df = df[df["aa_alt"] == "Ter"]
+	
+	df = df.sort_values(by = ["simple_name"]).reset_index(drop = True)
 
 	df.to_csv("/net/data/aasubs/clinvar_only/pvs1/stop_entries.csv")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 	main()
